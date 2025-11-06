a23ba6c8c12b0d4cd0e1e8caaa95c3a9aed4899d
Alejandro <alexchacon@gmail.com>
2025-11-06 18:06:37 +0100

    Add lista.py to define a fruit list and print its elements

---
+++ Unidad_03/structureControl/lista.py
@@ -0,0 +1,8 @@
+listaFrutas = ['pera','manzana','uva','platano','tomate']
+
+
+print('Primer elemento de la lista:',listaFrutas[0])
+print('Ultimo elemento de la lista:',listaFrutas[-1])
+
+for fruta in listaFrutas:
+    print(fruta)
