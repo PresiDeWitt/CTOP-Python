f952d58bdba4d2a2342f128df50b2a4fded44901
Alejandro <alexchacon@gmail.com>
2025-11-06 18:06:42 +0100

    Add parametros.py to demonstrate data type conversion and user input handling

---
+++ Unidad_03/structureControl/parametros.py
@@ -0,0 +1,11 @@
+print("Primer apartedo:")
+num = float(input("Ingrese un número decimal: "))
+
+print("El número ingresado es:", num)
+print("El tipo de dato es:", int(num))
+print("El tipo de dato es:", str(num))
+
+print("\nSegundo apartado:")
+texto = str(input("Ingrese un texto: "))
+suma= int(texto) + 2
+print("El resultado es:", suma)
